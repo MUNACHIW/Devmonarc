45db62a6bcd34cfdd2c1f1b05184361a58bf8414
MUNACHIW <munasom25@gmail.com>
2024-07-27 23:07:03 +0100

    github modifications

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/DevMonarch/",
+  base: "/Devmonarchinit/",
   plugins: [react()],
 })
